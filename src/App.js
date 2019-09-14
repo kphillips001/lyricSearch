9d3b6c7eb7ddda19f2daac7a12808ad6a5cfacd8
Kevin <knphillips01@gmail.com>
2019-09-14 14:56:17 -0500

    first commit, set up navBar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component } from 'react';
+import Navbar from './components/layout/navbar'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+      </div>
+    )
+  }
+}
+export default App;
