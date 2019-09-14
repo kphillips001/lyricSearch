52b3e74ab8fa236b254b7d0b8e551fffe564c011
Kevin <knphillips01@gmail.com>
2019-09-14 15:14:06 -0500

    added index and tracks components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, {Component } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/navbar'
+import Index from './components/layout/index'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Navbar />
-      </div>
+      <Router>
+        <>
+          <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/'component={Index} />
+              </Switch>
+            </div>
+        </>
+      </Router>
     )
   }
 }
---
+++ src/components/layout/index.js
@@ -0,0 +1,11 @@
+ import React from 'react';
+ import Tracks from '../tracks/tracks'
+ 
+ const Index = () => {
+   return (
+    <>
+      <Tracks />
+    </>
+   )
+ }
+  export default Index;
