1aeee6b99f198452a7e065f398208b3265d46497
Kevin <knphillips01@gmail.com>
2019-09-14 15:55:31 -0500

    finished building out context api and set up env variable

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React, {Component } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/navbar'
 import Index from './components/layout/index'
+
+import {Provider} from './context';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <>
-          <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/'component={Index} />
-              </Switch>
-            </div>
-        </>
-      </Router>
+      <Provider>
+        <Router>
+          <>
+            <Navbar />
+              <div className='container'>
+                <Switch>
+                  <Route exact path='/'component={Index} />
+                </Switch>
+              </div>
+          </>
+        </Router>
+      </Provider>
     )
   }
 }
--- src/context.js
+++ src/context.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
 export class Provider extends Component {
   state = {
-    track_list: [
-      { track: { track_name:'abc' } },
-      { track: { track_name:'123' } }
-    ],
+    track_list: [],
     heading: 'Top 10 Tracks'
   };
+
+  componentDidMount() {
+    axios.get(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+    .then(res => console.log(res.data))
+    .catch(err =>console.log(err));
+  }
   
   render() {
     return (
