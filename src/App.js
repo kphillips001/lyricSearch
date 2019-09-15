62c8134877d32479da70be60622d2b95f7b4f205
Kevin <knphillips01@gmail.com>
2019-09-15 18:53:50 -0500

    set up axios request for lyrics component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Navbar from './components/layout/navbar'
-import Index from './components/layout/index'
+import Navbar from './components/layout/navbar';
+import Index from './components/layout/index';
+import Lyrics from './components/tracks/lyrics';
 
 import {Provider} from './context';
 import './App.css';
@@ -16,6 +17,7 @@ class App extends Component {
               <div className='container'>
                 <Switch>
                   <Route exact path='/'component={Index} />
+                  <Route exact path='/lyrics/track/:id'component={Lyrics} />
                 </Switch>
               </div>
           </>
---
+++ src/components/tracks/lyrics.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+class lyrics extends Component {
+  state = {
+    track: {},
+    lyrics: {}
+  }
+
+  componentDidMount() {
+    axios
+    .get(
+      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+    )
+    .then(res => {
+      console.log(res.data)
+      //this.setState({track_list: res.data.message.body.track_list});
+      })
+    .catch(err =>console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Lyrics</h1>
+      </div>
+    )
+  }
+}
+
+export default lyrics;
