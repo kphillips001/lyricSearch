9760c1ccedecdd2c1943f565ab132d1d56f2a04a
Kevin <knphillips01@gmail.com>
2019-09-15 20:26:29 -0500

    finished lyrics component

--- src/components/tracks/lyrics.js
+++ src/components/tracks/lyrics.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Spinner from '../layout/spinner';
 
 class lyrics extends Component {
   state = {
@@ -13,19 +15,57 @@ class lyrics extends Component {
       `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
     )
     .then(res => {
-      console.log(res.data)
-      //this.setState({track_list: res.data.message.body.track_list});
-      })
+     this.setState({ lyrics: res.data.message.body.lyrics });
+
+     return axios 
+     .get(
+      `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+      );
+    })
+    .then(res => {
+      this.setState({ track: res.data.message.body.track });
+    })
     .catch(err =>console.log(err));
   }
 
   render() {
-    return (
-      <div>
-        <h1>Lyrics</h1>
-      </div>
-    )
-  }
+    const { track, lyrics } = this.state;
+    console.log(track)
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />
+    } else {
+      return (
+      <>
+       <Link to='/' className='btn btn-dark btn-sm mb-4'>Go Back</Link>
+       <div className='card'>
+        <h5 className='card-header'>
+          {track.track_name} by {''}
+          <span className='text-secondary'>{track.artist_name}</span>
+        </h5>
+        <div className='card-body'>
+          <p className='card-text'>{lyrics.lyrics_body}</p>
+        </div>
+       </div>
+       <ul className='list-group mt-3'>
+         <li className='list-group-item'>
+           <strong>Album ID</strong>: {track.album_id}
+         </li>
+         <li className='list-group-item'>
+           <strong>Song Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+         </li>
+         <li className='list-group-item'>
+          <strong>Explicit Words</strong>: {track.explicit === 0 ? 'No' : 'Yes'} 
+         </li>
+       </ul>
+      </>
+      );
+    }
+  } 
 }
 
 export default lyrics;
